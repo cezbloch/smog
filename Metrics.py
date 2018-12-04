669ba704e2d58946b7448e0c581cf146f90506ed
Cezary Bloch <35893209+cezbloch@users.noreply.github.com>
2018-12-04 22:40:45 +0100

    Add new simpler Gui app

--- Metrics.py
+++ Metrics.py
@@ -19,18 +19,30 @@ class Metrics(object):
 
 breakpoints_2_5 = (0.0, 12.0, 35.5, 55.5, 150.0, 250.0, 350.0, 500.5)
 breakpoints_10 = (0.0, 55.0, 155.0, 255.0, 355.0, 425.0, 505.0, 605.0)
-index_breakpoints = (0, 51, 101, 151, 201, 301, 401, 501)
+breakpoints_aqi = (0, 51, 101, 151, 201, 301, 401, 501)
 labels = ("Good", "Moderate", "Unhealthy for Sensitive Groups", "Unhealthy", "Very Unhealthy", "Hazardous", "Death")
 colors = ("green", "yellow", "orange", "red", "purple", "maroon", "maroon", "black")
 
 
-def get_aqi_color(aqi):
+def get_color(value, breakpoints):
     index = 0
-    for low in index_breakpoints:
-        if aqi < low:
+    for low in breakpoints:
+        if value < low:
             return colors[index]
         index += 1
     raise IndexError
+
+
+def get_2_5_color(value):
+    return get_color(value, breakpoints_2_5)
+
+
+def get_10_color(value):
+    return get_color(value, breakpoints_10)
+
+
+def get_aqi_color(value):
+    return get_color(value, breakpoints_aqi)
 
 
 def calculate_aqi(value, levels):
@@ -39,8 +51,8 @@ def calculate_aqi(value, levels):
         if value <= level:
             c_low = levels[index - 1]
             c_high = levels[index]
-            i_low = index_breakpoints[index - 1]
-            i_high = index_breakpoints[index] - 1
+            i_low = breakpoints_aqi[index - 1]
+            i_high = breakpoints_aqi[index] - 1
             aqi = (i_high - i_low) * (value - c_low) / (c_high - c_low) + i_low
             return aqi
         index += 1
