8f349eed96e107d67f2af65b9e8cfee6b4ef642d
Cezary Bloch <35893209+cezbloch@users.noreply.github.com>
2018-12-04 23:07:46 +0100

    Gui cleanup for better visual effect

--- AirMonitorGui.py
+++ AirMonitorGui.py
@@ -8,9 +8,9 @@ from Metrics import get_aqi_color, get_2_5_color, get_10_color
 from DataTargets import create_data_target
 
 
-class simpleapp_tk(Tkinter.Tk):
+class AirMonitorGui(Tkinter.Tk):
     def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
+        Tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
 
@@ -34,38 +34,41 @@ class simpleapp_tk(Tkinter.Tk):
         column_0 = 0
         column_1 = 1
         column_2 = 2
-        self.label7 = Tkinter.Label(self, anchor="center", bg="black")
-        self.label7.grid(column=column_0, row=row_2, sticky='NESW')
+        text_font = ("Courier", 50)
 
-        self.label8 = Tkinter.Label(self, anchor="center", bg="black")
-        self.label8.grid(column=column_1, row=row_2, sticky='NESW')
+        self.label_aqi = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_aqi.grid(column=column_0, row=row_0, sticky='NESW')
 
-        self.label9 = Tkinter.Label(self, anchor="center", bg="black")
-        self.label9.grid(column=column_2, row=row_2, sticky='NESW')
+        self.label_pm25 = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm25.grid(column=column_1, row=row_0, sticky='NESW')
 
-        self.label = Tkinter.Label(self,anchor="center",bg="black")
-        self.label.grid(column=column_0,row=row_0,sticky='NESW')
+        self.label_pm10 = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm10.grid(column=column_2, row=row_0, sticky='NESW')
 
-        self.label2 = Tkinter.Label(self,anchor="center",bg="black")
-        self.label2.grid(column=column_1,row=row_0,sticky='NESW')
+        self.label_aqi_now = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_aqi_now.grid(column=column_0, row=row_1, sticky='NESW')
 
-        self.label3 = Tkinter.Label(self,anchor="center",bg="black")
-        self.label3.grid(column=column_2,row=row_0,sticky='NESW')
+        self.label_pm25_now = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm25_now.grid(column=column_1, row=row_1, sticky='NESW')
 
-        self.label4 = Tkinter.Label(self,anchor="center",bg="black")
-        self.label4.grid(column=column_0,row=row_1,sticky='NESW')
+        self.label_pm10_now = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm10_now.grid(column=column_2, row=row_1, sticky='NESW')
 
-        self.label5 = Tkinter.Label(self,anchor="center",bg="black")
-        self.label5.grid(column=column_1,row=row_1,sticky='NESW')
+        self.label_aqi_avg = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_aqi_avg.grid(column=column_0, row=row_2, sticky='NESW')
 
-        self.label6 = Tkinter.Label(self,anchor="center",bg="black")
-        self.label6.grid(column=column_2,row=row_1,sticky='NESW')
+        self.label_pm25_avg = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm25_avg.grid(column=column_1, row=row_2, sticky='NESW')
 
-        self.grid_columnconfigure(column_0,weight=1)
-        self.grid_columnconfigure(column_1,weight=1)
-        self.grid_columnconfigure(column_2,weight=1)
-        self.grid_rowconfigure(row_0,weight=1)
-        self.grid_rowconfigure(row_1,weight=1)
+        self.label_pm10_avg = Tkinter.Label(self, anchor="center", bg="black", font=text_font)
+        self.label_pm10_avg.grid(column=column_2, row=row_2, sticky='NESW')
+
+        self.grid_columnconfigure(column_0, weight=1)
+        self.grid_columnconfigure(column_1, weight=1)
+        self.grid_columnconfigure(column_2, weight=1)
+        self.grid_rowconfigure(row_0, weight=1)
+        self.grid_rowconfigure(row_1, weight=1)
+        self.grid_rowconfigure(row_2, weight=1)
 
     def update_result(self, smog_values):
         self.smog_values = smog_values
@@ -77,27 +80,27 @@ class simpleapp_tk(Tkinter.Tk):
             color_2_5 = get_2_5_color(result.momentarily[0])
             color_10 = get_10_color(result.momentarily[1])
             aqi_color = get_aqi_color(result.momentarily[2])
-            self.label.config(text="%.1f" % result.momentarily[0], fg=color_2_5)
-            self.label2.config(text="%.1f" % result.momentarily[1], fg=color_10)
-            self.label3.config(text="%.1f" % result.momentarily[2], fg=aqi_color)
+            self.label_aqi_now.config(text="%d" % result.momentarily[2], fg=aqi_color)
+            self.label_pm25_now.config(text="%.1f" % result.momentarily[0], fg=color_2_5)
+            self.label_pm10_now.config(text="%.1f" % result.momentarily[1], fg=color_10)
 
-            self.label7.config(text="PM 2.5", fg="white")
-            self.label8.config(text="PM 10", fg="white")
-            self.label9.config(text="AQI", fg="white")
+            self.label_aqi.config(text=" AQI ", fg="white")
+            self.label_pm25.config(text=" PM 2.5 ", fg="white")
+            self.label_pm10.config(text=" PM 10 ", fg="white")
 
             if result.average is not None:
                 color_2_5_avg = get_2_5_color(result.average[0])
                 color_10_avg = get_10_color(result.average[1])
                 aqi_color_avg = get_aqi_color(result.average[2])
-                self.label4.config(text="%.1f" % result.average[0], fg=color_2_5_avg)
-                self.label5.config(text="%.1f" % result.average[1], fg=color_10_avg)
-                self.label6.config(text="%.1f" % result.average[2], fg=aqi_color_avg)
+                self.label_aqi_avg.config(text="%d" % result.average[2], fg=aqi_color_avg)
+                self.label_pm25_avg.config(text="%.1f" % result.average[0], fg=color_2_5_avg)
+                self.label_pm10_avg.config(text="%.1f" % result.average[1], fg=color_10_avg)
 
         self.after(33, self.update_ui)
 
 
 if __name__ == "__main__":
-    app = simpleapp_tk(None)
+    app = AirMonitorGui(None)
     app.title("Air Quality Monitor")
     app.after(33, app.update_ui)
     app.mainloop()
