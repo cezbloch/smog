c9aaf087cfb9baa3c51cc9abf9f8d68d3584eda4
Cezary Bloch <cezary.bloch@displaylink.com>
2018-12-07 21:47:19 +0100

    On linux sometimes the device port drops and reappears with consecutive id eg. /dev/ttyusb0 drops to reappear as /dev/ttyusb1 This change adds mechanism to recreate serial port object on failure with next port ID. Reconnection retries with ID from 0-9, each ID on next retry. When exceed 9 it starts again from 0.

--- SmogMeters.py
+++ SmogMeters.py
@@ -28,15 +28,28 @@ class AqiMeterDecorator(object):
 
 class SDL607(object):
     def __init__(self, com_port):
-        self.serial_port = serial.Serial(com_port, 9600, timeout=0, parity=serial.PARITY_NONE, stopbits=1, bytesize=8)
+        self.com_port = com_port
+        self.port_name = self.com_port[:-1]
+        self.port_number = 0
         self.buffer = ""
         self.inputPattern = re.compile('\\xAA.{17}\\xAB', re.DOTALL)
+        self.serial_port = serial.Serial(self.com_port, 9600, timeout=0, parity=serial.PARITY_NONE, stopbits=1, bytesize=8)
+
+    def reconnect(self):
+        try:
+            self.com_port = self.port_name + str(self.port_number)
+            self.port_number += 1
+            if self.port_number > 10:
+                self.port_number = 0
+            self.serial_port = serial.Serial(self.com_port, 9600, timeout=0, parity=serial.PARITY_NONE, stopbits=1, bytesize=8)
+        except IOError as error:
+            print "SDL607 failed to reconnect on port {} with error: {}".format(self.com_port, str(error))
 
     def get_values(self):
         try:
             while self.serial_port.inWaiting() > 0:
-                l = self.serial_port.inWaiting()
-                self.buffer += self.serial_port.read(l)
+                bytes_amount = self.serial_port.inWaiting()
+                self.buffer += self.serial_port.read(bytes_amount)
                 m = self.inputPattern.match(self.buffer)
                 if m:
                     data = self.buffer[m.start():m.end()]
@@ -55,6 +68,7 @@ class SDL607(object):
                 return None
         except IOError as error:
             print "SDL607 failed to read value with error: {}".format(str(error))
+            self.reconnect()
 
 
 class FakeMeter(object):
