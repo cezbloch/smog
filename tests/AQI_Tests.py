152bd8f19b39ec01a4c189655d3f3ebdaa3018a5
Cezary Bloch <cezary.bloch@displaylink.com>
2018-12-17 09:01:17 +0100

    Adding unit test for correct color retrieval

--- tests/AQI_Tests.py
+++ tests/AQI_Tests.py
@@ -1,5 +1,5 @@
 import unittest
-from Metrics import AQI
+from Metrics import AQI, get_2_5_color
 
 
 class TestAQI(unittest.TestCase):
@@ -13,6 +13,10 @@ class TestAQI(unittest.TestCase):
         aqi = AQI(12, 10)
         index = aqi.get_index()
         self.assertEqual(index, 50)
+
+    def test_gets_correct_color_for_good_quality(self):
+        color = get_2_5_color(8.0)
+        self.assertEqual(color, "green")
 
 
 if __name__ == '__main__':
